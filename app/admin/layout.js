5ff8bcc9bca07a773cde39cbc477363e780f0adc
Prinze Amir <myprinzeamir@gmail.com>
2024-03-18 10:14:56 -0400

    testing chakra ui

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,20 +1,26 @@
 import adminStyles from './admin.module.css'
 import Link from 'next/link'
+import { ChakraProvider } from '@chakra-ui/react'
+
 export default function AdminLayout({ children }) {
 
   return (
-   <section className={adminStyles.adminContiainer}>
-    <div id="adminNav" className={adminStyles.nav}>
-                <ul>
-                    <Link href="/admin"><li>Home</li></Link>
-                    <li>Recipients</li>
-                    <li>Donors</li>
-                    <Link href="/admin/settings"><li>Settings</li></Link>                    
-                    <li><input placeholder="Search Recipients"/></li>
+    <ChakraProvider>
+        <section className={adminStyles.adminContiainer}>
+                <div id="adminNav" className={adminStyles.nav}>
+                    <ul>
+                        <Link href="/admin"><li>Home</li></Link>
+                        <li>Recipients</li>
+                        <li>Donors</li>
+                        <Link href="/admin/settings"><li>Settings</li></Link>                    
+                        <li><input placeholder="Search Recipients"/></li>
 
-                </ul>
-            </div>
-    {children}
-   </section>
+                    </ul>
+                </div>
+          {children}
+        </section>
+    </ChakraProvider>
+  
+   
   )
 }
--- app/admin/settings/page.js
+++ app/admin/settings/page.js
@@ -1,7 +1,10 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 export default function Settings() {
     return (
         <div>
             <h1>Settings</h1>
-        </div>
+            <Button colorScheme='blue'>Button</Button>        
+            </div>
     )
 }
